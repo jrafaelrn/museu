18eb5fc3e00028ec01dce75f9e4b6d342802fdba
J Rafael R Nascimento <63913814+jrafaelrn@users.noreply.github.com>
2022-10-29 21:49:32 -0300

    Fix Python install and implemetation OpenCv Upscale

--- 2-Upscale/opencv/main.py
+++ 2-Upscale/opencv/main.py
@@ -1,1 +1,21 @@
-print("Hello World")
+import cv2
+from cv2 import dnn_superres
+
+# Create an SR object
+sr = dnn_superres.DnnSuperResImpl_create()
+
+# Read image
+image = cv2.imread('./temp/0.png')
+
+# Read the desired model
+path = "./models/EDSR_x3.pb"
+sr.readModel(path)
+
+# Set the desired model and scale to get correct pre- and post-processing
+sr.setModel("edsr", 3)
+
+# Upscale the image
+result = sr.upsample(image)
+
+# Save the image
+cv2.imwrite("./temp/0-up.png", result)
